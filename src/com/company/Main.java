0e3f0aa9c9a68d9696696885749cda15530a5c67
BT_1N3_26@BKTECH.LOCAL <BT_1N3_26@BKTECH.LOCAL>
2018-10-26 11:45:18 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print(" ");
+	    for (int i = 0; i < 13; i++)
+        {
+            System.out.print("_");
+        }
+        System.out.println();
+        System.out.print("/");
+        for (int i = 0; i < 13; i++)
+        {
+            System.out.print("|");
+        }
+        System.out.print("\\");
+        System.out.println();
+        for (int i = 0; i < 4; i++)
+        {
+            System.out.print("|");
+        }
+        System.out.print("O");
+        for (int i = 0; i < 5; i++)
+        {
+            System.out.print("|");
+        }
+        System.out.print("O");
+        for (int i = 0; i < 4; i++)
+        {
+            System.out.print("|");
+        }
+        System.out.println();
+        for (int i = 0; i < 7; i++)
+        {
+            System.out.print("|");
+        }
+        System.out.print("U");
+        for (int i = 0; i < 7; i++)
+        {
+            System.out.print("|");
+        }
+        System.out.println();
+        for (int i = 0; i < 15; i++)
+        {
+            System.out.print("|");
+        }
+        System.out.println();
+        System.out.print("\\");
+        for (int i = 0; i < 13; i++)
+        {
+            System.out.print("_");
+        }
+        System.out.print("/");
+    }
+}
